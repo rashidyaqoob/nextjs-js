037bee22747f47b028aab6fb6cd8c236afc098b4
Rashid <rashid.yaqoob@axelerant.com>
2024-01-03 14:38:23 +0530

    Create componnets

---
+++ next-app/src/app/components/Accordion.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Accordion} from "@nextui-org/react";
+
+export default function App() {
+  const defaultContent =
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.";
+
+  return (
+    <Accordion>
+      
+    </Accordion>
+  );
+}
