4ad35781a5ec0f73399f3eb37e1ffbb752b3d618
Rashid <rashid.yaqoob@axelerant.com>
2024-01-03 13:01:23 +0530

    Create next app

---
+++ next-app/src/app/components/AddToCart.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react'
+
+const AddToCart = () => {
+  return (
+    <button onClick={()=> console.log('hello')}>Add to cart</button>
+  )
+}
+
+export default AddToCart
---
+++ next-app/src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+import ProductCard from './components/ProductCard'
+
+export default function Home() {
+  return (
+    <main><h1>Hello world</h1>
+    <ProductCard/></main>
+  )
+}
