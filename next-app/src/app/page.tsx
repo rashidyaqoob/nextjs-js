559cbf8d156c9d0420c8b3c6435dfff5808438e0
Rashid <rashid.yaqoob@axelerant.com>
2024-01-03 13:53:37 +0530

    Fetch users, style using tailwing

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
-import ProductCard from './components/ProductCard'
+import ProductCard from './components/ProductCard';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main><h1>Hello world</h1>
+    <Link href='/users'>Users</Link>
     <ProductCard/></main>
   )
 }
